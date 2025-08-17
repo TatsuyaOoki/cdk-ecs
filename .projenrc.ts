d65343d30060a0e64c6d0868e9e2217b9973693b
TatsuyaOoki <tat.ooki@outlook.jp>
2025-08-17 14:21:37 +0000

    chore: add jestOptions configuration for snapshot updates

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,15 @@
 import { awscdk } from "projen";
+import { UpdateSnapshot } from "projen/lib/javascript";
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: "2.1.0",
   defaultReleaseBranch: "main",
   name: "cdk-ecs",
   projenrcTs: true,
   buildWorkflow: false,
-
+  jestOptions: {
+    updateSnapshot: UpdateSnapshot.NEVER,
+  },
   deps: ["dotenv"] /* Runtime dependencies of this module. */,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
