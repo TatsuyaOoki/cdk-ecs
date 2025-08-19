86108b79c1e8b8185f48562480706ebcf2aa1458
TatsuyaOoki <tat.ooki@outlook.jp>
2025-08-19 13:23:32 +0000

    fix: update repository and project name in development parameters

--- src/parameter.ts
+++ src/parameter.ts
@@ -17,8 +17,8 @@ export const devParameter: AppParameter = {
     region: process.env.CDK_DEPLOY_REGION,
   },
   envName: "Develop",
-  repository: "TatsuyaOoki/cdk-aws-network",
-  projectName: "WebStandalone",
+  repository: "TatsuyaOoki/cdk-ecs",
+  projectName: "CdkEcs",
 
-  vpcCidr: "10.1.0.0/16",
+  vpcCidr: "172.16.0.0/16",
 };
